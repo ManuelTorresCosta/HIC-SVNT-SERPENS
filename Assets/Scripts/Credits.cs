6c58d8e33fbab99ddc882aff4af1a719eae00026
Manuel Costa <73121617+manuel-costa@users.noreply.github.com>
2023-09-07 18:30:57 +0100

    Tweaked some animations and transitions

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -7,48 +7,38 @@ using UnityEngine.UI;
 public class Credits : MonoBehaviour
 {
     public Text inputText;
-
-    private bool canBlink = false;
-    private float timer = 0;
     public float blinkSpeed = 1f;
+
+    private float _timer = 0f;
+    private bool _canBlink = false;
 
 
     private void Awake()
     {
         inputText.gameObject.SetActive(false);
     }
-    private void Start()
-    {
-        StartCoroutine(StartTimer());
-    }
     private void Update()
     {
         if (Input.anyKey)
             SceneManager.LoadScene(0);
 
-        if (canBlink)
+        if (_canBlink)
         {
-            Blink();
+            if (_timer < 1)
+                _timer += blinkSpeed * Time.deltaTime;
+            else
+            {
+                inputText.gameObject.SetActive(!inputText.gameObject.activeSelf);
+                _timer = 0;
+            }
         }
     }
 
-
-
-    private IEnumerator StartTimer()
-    {
-        yield return new WaitForSeconds(10f);
-        inputText.gameObject.SetActive(true);
-        canBlink = true;
-
-    }
+    
     private void Blink()
     {
-        if (timer < 1)
-            timer += blinkSpeed * Time.deltaTime;
-        else
-        {
-            inputText.gameObject.SetActive(!inputText.gameObject.activeSelf);
-            timer = 0;
-        }
+        if (!_canBlink)
+            _canBlink = true;
+        
     }
 }
